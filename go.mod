a0ab9f6cfa79944feeecfa9ff317e2225e81e047
chengqinglin <chengqinglin@icloud.com>
2023-05-31 23:43:23 +0800

    chore unittest
    Signed-off-by: chengqinglin <chengqinglin@icloud.com>

--- go.mod
+++ go.mod
@@ -60,6 +60,7 @@ require (
 	github.com/teivah/onecontext v0.0.0-20200513185103-40f981bfd775 // indirect
 	github.com/xlab/treeprint v1.1.0 // indirect
 	go.starlark.net v0.0.0-20200306205701-8dd3e2ee1dd5 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/oauth2 v0.5.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
--- test/envtest/environment_test.go
+++ test/envtest/environment_test.go
@@ -2,12 +2,17 @@ package envtest
 
 import (
 	"context"
-	"fmt"
+	"embed"
+	"encoding/json"
+	"io/fs"
+	"strings"
 	"testing"
 
+	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
@@ -16,8 +21,55 @@ import (
 	"github.com/imuxin/ksql/pkg/repl"
 )
 
-func PrepareDatabase(_ *rest.Config) {
+func getAllFilenames(efs *embed.FS) (files []string, err error) {
+	if err := fs.WalkDir(efs, ".", func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			return nil
+		}
 
+		files = append(files, path)
+
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return files, nil
+}
+
+//go:embed testdata/*
+var content embed.FS
+
+type resource struct {
+	APIVersion string                 `json:"apiVersion"`
+	Kind       string                 `json:"kind"`
+	Metadata   map[string]interface{} `json:"metadata"`
+	Spec       map[string]interface{} `json:"spec,omitempty"`
+}
+
+func prepareDatabase(t *testing.T, restConfig *rest.Config) {
+	restConfig.APIPath = "api"
+	restConfig.NegotiatedSerializer = scheme.Codecs
+	data, _ := content.ReadFile("testdata/data.yaml")
+	for _, item := range strings.Split(string(data), "---") {
+		r := &resource{}
+		assert.NoError(t, yaml.Unmarshal([]byte(item), r))
+		body, err := json.Marshal(r)
+		assert.NoError(t, err)
+		gv, err := schema.ParseGroupVersion(r.APIVersion)
+		assert.NoError(t, err)
+		restConfig.GroupVersion = &gv
+
+		restCli, err := rest.RESTClientFor(restConfig)
+		assert.NoError(t, err)
+
+		req := restCli.Post().Resource(strings.ToLower(r.Kind) + "s").Body(body)
+		if r.Metadata["namespace"] != nil {
+			req = req.Namespace(r.Metadata["namespace"].(string))
+		}
+		err = req.Do(context.TODO()).Error()
+		assert.NoError(t, err)
+	}
 }
 
 func TestExecuteAndFormat(t *testing.T) {
@@ -25,22 +77,24 @@ func TestExecuteAndFormat(t *testing.T) {
 	restConfig, err := env.Start()
 	assert.NoError(t, err)
 
-	n := &corev1.Namespace{}
-	n.Name = "ksql"
+	prepareDatabase(t, restConfig)
 
-	restConfig.GroupVersion = &corev1.SchemeGroupVersion
-	restConfig.APIPath = "api"
-	restConfig.NegotiatedSerializer = scheme.Codecs
-
-	restCli, err := rest.RESTClientFor(restConfig)
+	files, err := getAllFilenames(&content)
 	assert.NoError(t, err)
 
-	err = restCli.Post().Resource("namespaces").Body(n).Do(context.TODO()).Error()
-	assert.NoError(t, err)
-
-	result, err := executor.Execute[unstructured.Unstructured]("select * from namespace", restConfig)
-	assert.NoError(t, err)
-	fmt.Println(repl.Output(result))
+	sqls := lo.Filter(files, func(item string, index int) bool {
+		return strings.HasSuffix(item, ".sql")
+	})
+	lo.ForEach(sqls, func(item string, _ int) {
+		b, err := content.ReadFile(item)
+		assert.NoError(t, err)
+		result, err := executor.Execute[unstructured.Unstructured](string(b), restConfig)
+		assert.NoError(t, err)
+		expect, err := content.ReadFile(strings.TrimSuffix(item, ".sql") + ".output")
+		assert.NoError(t, err)
+		// fmt.Println(repl.Output(result))
+		assert.Equal(t, string(expect), repl.Output(result))
+	})
 
 	assert.NoError(t, env.Stop())
 }
